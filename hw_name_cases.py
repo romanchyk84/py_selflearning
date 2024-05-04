b48d2a87440a3faac9fdefbbcd529f8ff6fbd0b8
Roman Motorin <romanchik84@gmail.com>
2024-05-04 12:46:12 +0300

    hw#1 strings added

---
+++ hw_name_cases.py
@@ -0,0 +1,29 @@
+name = "Roman"
+print(f'Hello, {name}, would you like to learn some Python today?')
+print('*'*30)
+
+
+name = 'anTAnAnaRIVu'
+print(name.title())
+print(name.lower())
+print(name.upper())
+print('-'*50)
+
+
+phrase = 'Albert Einstein once said, "A person who never made  a mistake never tried anything new."'
+print(phrase)
+phrase1 = "Albert Einstein once said, \"A person who never made  a mistake never tried anything new.\""
+print(phrase1)
+
+famous_person = 'Albert Einstein'
+message = 'A person who never made  a mistake never tried anything new'
+print(f"{famous_person} once said, \"{message}\".\n\t{famous_person} once said, '{message}'.")
+
+
+user = '  Roman Motorin  '
+print(user)
+print(user.lstrip())
+print(user.rstrip())
+print(user.strip())
+
+
