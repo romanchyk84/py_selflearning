ebb642dd77fdad5ed18c27696f5a4eb29a8322a9
Roman Motorin <romanchik84@gmail.com>
2024-05-14 00:36:56 +0300

    hw with dictionary

--- hw_dicts.py
+++ hw_dicts.py
@@ -8,3 +8,21 @@ print(f"{man['first_name'].title()}'s last name is {man['last_name'].title()}")
 print(f"{man['first_name'].title()} {man['last_name'].title()} is {man['age']} y.o.")
 print(f"He lives in the city of {man['city'].title()}")
 
+print('\n', '-'*15, 'The end', '-'*15, '\n')
+
+fav_numbers = {
+       'roman': 5,
+       'ilona': 10,
+       'sasha': 15,
+       'maksim': 20,
+       'olya': 25,
+       }
+print(f"Roman's favorite number is {fav_numbers['roman']}")
+print(f"{fav_numbers['ilona']} is Ilona's favorite number")
+print(f"Sasha's favorite number is {fav_numbers['sasha']}")
+print(f"{fav_numbers['maksim']} is Maksim's favorite number")
+print(f"Olya's favorite number is {fav_numbers['olya']}")
+
+print(f'\n', '*'*5, 'Previous task using cycle for', '*'*5, '\n')
+for key in fav_numbers:
+       print(f"{key.title()}'s favorite number is {fav_numbers[key]}")
