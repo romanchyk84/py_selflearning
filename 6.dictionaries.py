0f2e63efbe0a510b4ca56f6bbbac65fc7d319fd7
Roman Motorin <romanchik84@gmail.com>
2024-05-13 22:59:32 +0300

    Dictionaries updated

--- 6.dictionaries.py
+++ 6.dictionaries.py
@@ -10,3 +10,13 @@ print(f"Your just earned {new_points} points!")
 alien_0['x_position'] = 0
 alien_0['y_position'] = 25
 
+# del alien_0
+
+alien_1 = {}    # Создание пустого словаря
+alien_1['color'] = 'red'    # Добавление первой пары "ключ": "значение"
+alien_1['points'] = 10      # Добавление второй пары "ключ": "значение"
+print(f"Alien color is {alien_1['color']}")     # Вывод текущего цвета
+
+alien_1['color'] = 'yellow'     # Изменение первой пары, меняем значение цвета с green на yellow
+print(f"New alien color is {alien_1['color']}")
+
