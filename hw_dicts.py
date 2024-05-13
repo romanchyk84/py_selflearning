981aeeeae9aff9fcfc46da5ba7954d2c5ce7f276
Roman Motorin <romanchik84@gmail.com>
2024-05-14 00:14:27 +0300

    hw with dictionary

---
+++ hw_dicts.py
@@ -0,0 +1,10 @@
+man = {'first_name': 'jason',
+       'last_name': 'statham',
+       'age': 56,
+       'city': 'london',
+       }
+print(f"His name is {man['first_name'].title()}")
+print(f"{man['first_name'].title()}'s last name is {man['last_name'].title()}")
+print(f"{man['first_name'].title()} {man['last_name'].title()} is {man['age']} y.o.")
+print(f"He lives in the city of {man['city'].title()}")
+
