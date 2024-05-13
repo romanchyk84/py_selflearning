06ccda168a3bf649563b85902a70884f6c1b5ede
Roman Motorin <romanchik84@gmail.com>
2024-05-13 22:38:50 +0300

    Dictionaries

---
+++ 6.dictionaries.py
@@ -0,0 +1,12 @@
+
+# Словари
+alien_0 = {'color': 'green', 'points': 5, 'key': 'value'}
+print(alien_0['color'])     # Вывводим значение по ключу color
+print(alien_0['points'])    # Выводим значение по ключу points
+
+new_points = alien_0['points']
+print(f"Your just earned {new_points} points!")
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+
