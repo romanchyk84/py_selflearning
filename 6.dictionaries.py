c3e27be0be4c2da9894ee728e18d3f51b7720fef
Roman Motorin <romanchik84@gmail.com>
2024-05-13 23:19:44 +0300

    Dictionaries updated

--- 6.dictionaries.py
+++ 6.dictionaries.py
@@ -20,3 +20,16 @@ print(f"Alien color is {alien_1['color']}")     # Вывод текущего ц
 alien_1['color'] = 'yellow'     # Изменение первой пары, меняем значение цвета с green на yellow
 print(f"New alien color is {alien_1['color']}")
 
+# Пришелец перемещается вправо.
+# Вычисляем величину смещения на основании текущей скорости.
+alien_2 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print(f"Original position: {alien_2['x_position']}")
+if alien_2['speed'] == 'slow':
+    x_increment = 1
+elif alien_2['speed'] == 'medium':
+    x_increment = 2
+else:
+    # Пришелец двигается быстро
+    x_increment = 3
+alien_2['x_position'] = alien_2['x_position'] + x_increment
+print(f"Новая позиция: {alien_2['x_position']}")
